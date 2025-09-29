f5135be1000eee1600698719dd6cb976b985199e
mateus-pereira-inteli <mateus.pereira@sou.inteli.edu.br>
2025-09-29 10:48:17 -0300

    feat: adcionar logo Nvidia na navbar

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -8,7 +8,11 @@ function Layout() {
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex items-center justify-between h-16">
             <div className="flex items-center">
-              <div className="ml-10 flex items-baseline space-x-4">
+              <div className="ml-10 flex items-center space-x-4">
+                <div className="flex items-center flex-shrink-0">
+                  <img src="/public/logo-nvidia.svg" alt="NVIDIA Logo" className="h-12 w-auto" />
+                </div>
+
                 <NavLink
                   to="/"
                   className={({ isActive }) =>
